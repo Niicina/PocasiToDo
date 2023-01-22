e1a800adee3a1a5c83cc3ed213c0259fc452279c
Nikola Valvodová <nikol.val.vodova@gmail.com>
2023-01-22 12:13:00 +0100

    zavolani environment - zavolani api key a api url Pridani http clienta k dostani odpovedi od Api pomocí get, volani parametru 'q' = jmeno mesta, druhy pararemtr appid(api key) Přidáno logování Konstukrtor - pridáno loaddata pro načtení při příchodu na homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http'
+
+//zavolani environment - zavolani api key a api url
+const API_URL = environment.API_URL;
+const API_KEY = environment.API_KEY;
 
 @Component({
   selector: 'app-home',
@@ -7,6 +13,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
 
+  constructor(public httpClient: HttpClient) {
+    this.loadData() //loaddata pokaždé když jdu an homepage
+  }
+
+  //vyuziti http klienta k dostani odpovedi od API pomocí get, prvni parametr q = jméno města, druhy parametr je api key
+  //results log - logování
+  loadData() {
+    this.httpClient.get('${API_URL}/weather?q = ${"Prague"}&appid=${API_KEY}').subscribe(results => { console.log(results);})
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  API_KEY: '7d94b78a680b8f82d2cd9d32df137398',
-  API_URL: 'https://api.openweathermap.org/data/2.5/'
+  API_KEY : '7d94b78a680b8f82d2cd9d32df137398',
+  API_URL : 'https://api.openweathermap.org/data/2.5/'
 };
 
 /*
