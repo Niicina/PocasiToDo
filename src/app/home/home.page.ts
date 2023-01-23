5a21be4021977e56376a683477ab80f4f206ee84
Nikola Valvodová <nikol.val.vodova@gmail.com>
2023-01-23 19:54:11 +0100

    viz predchozi commit, pridani ikon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
-
-
+import { Icon } from 'ionicons/dist/types/components/icon/icon';
+ 
 
 //zavolani environment - zavolani api key a api url
 const API_URL = environment.API_URL;
@@ -19,6 +19,10 @@ export class HomePage {
   //promenna teplota
   teplota : any
   dnesniDen = new Date
+  mesto: any
+  pocasiIkona: any 
+  pocasiDetail: any
+  path:any = "obrazek"
   constructor(public httpClient: HttpClient) {
     this.loadData() //loaddata pokaždé když jdu an homepage
   }
@@ -26,12 +30,16 @@ export class HomePage {
   //vyuziti http klienta k dostani odpovedi od API pomocí get, prvni parametr q = jméno města, druhy parametr je api key
   //results log - logování
   loadData() {
-    this.httpClient.get<any>(`${API_URL}/weather?q=${"Prague"}&appid=${API_KEY}`).subscribe(results => {
+    this.httpClient.get<any>(`${API_URL}/weather?q=${"Brno"}&appid=${API_KEY}`).subscribe(results => {
       console.log(results);
-      //dostani teploty z vysleddku od get - json odpoved 'temp'- v main polozka temperature a logovani
+      //dostani teploty z vysledku od get z API - json odpoved 'temp'- v main polozka temperature a logovani
       this.teplota = results['main']
+      this.mesto = results['name']
       console.log(this.teplota);
-     
+      this.pocasiIkona= results ['weather'][1]
+      console.log(this.pocasiDetail);
+      //ikona zavisi na tom jake je pocasi - z API
+      this.pocasiIkona= `http://openweathermap.org/img/wn/${this.pocasiDetail.icon}@2x.png`
      
 
     })
