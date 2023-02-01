c3fe277765d145e7a4552a8e017eac6b06cd439a
Nikola Valvodová <nikol.val.vodova@gmail.com>
2023-02-01 15:14:37 +0100

    Rozjeti appky v emulatoru

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
-};
+  production: false,
+  API_URL : 'https://api.openweathermap.org/data/2.5/',
+  API_KEY: '7d94b78a680b8f82d2cd9d32df137398',
+  images: 'http://openweathermap.org/img/wn/'
+};  production: true
+
