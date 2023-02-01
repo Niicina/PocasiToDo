fceb9b0053e015617c23c62921f166f7953348ab
Nikola Valvodová <nikol.val.vodova@gmail.com>
2023-02-01 20:15:17 +0100

    splash a ikona

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,27 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'splashNV',
   appName: 'PocasiToDo',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      //androidSpinnerStyle: "large",
+      //iosSpinnerStyle: "small",
+      //spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
