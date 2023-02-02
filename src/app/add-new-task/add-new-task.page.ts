7908e6580f936ea39670306c199dbb4d6a26ab89
Nikola Valvodová <nikol.val.vodova@gmail.com>
2023-02-02 18:04:17 +0100

    pridani zavreni modalniho ookna

--- src/app/add-new-task/add-new-task.page.ts
+++ src/app/add-new-task/add-new-task.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-new-task',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNewTaskPage implements OnInit {
 
-  constructor() { }
+  constructor(public modalCtrl: ModalController) { }
 
   ngOnInit() {
   }
-
+//mwtoda pro zavreni modalniho okna - pro krizek :)
+  async dismis(){
+  await this.modalCtrl.dismiss()
+  }
 }
