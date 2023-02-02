1b906d2d0bf9ccee609aedf9800cea79529b1103
Nikola Valvodová <nikol.val.vodova@gmail.com>
2023-02-02 21:36:08 +0100

    vygenerování servisky

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 //přidání locale cs pro datum v cs formátu
 import { LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localeCs from '@angular/common/locales/cs';
-
+//pridani importu pro angular storage
 registerLocaleData(localeCs);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot()],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, {provide:LOCALE_ID, useValue: "cs"  }],
   bootstrap: [AppComponent],
 })
