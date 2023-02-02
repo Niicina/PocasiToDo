95d039e86628f5469b984781f747098f6cb2ca25
Nikola Valvodová <nikol.val.vodova@gmail.com>
2023-02-02 12:22:43 +0100

    Tvorba todo - vytvoření kategorií defaultních (zatím), zobrazení jednotlivých itemů z todolistu v html

--- src/app/todo/todo.page.ts
+++ src/app/todo/todo.page.ts
@@ -7,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoPage implements OnInit {
 
+  todoList = [{
+    itemName: 'PrvnitoDo',
+    itemDueDate: '11-01-2022',
+    itemPriority: "vysoka",
+    itemCategory: 'Prace'
+
+  },
+  {
+    itemName: 'PrvnitoDo',
+    itemDueDate: '12-01-2022',
+    itemPriority: "mala",
+    itemCategory: 'Skola'
+
+  },
+  {
+    itemName: 'PrvnitoDo',
+    itemDueDate: '14-01-2022',
+    itemPriority: "stredni",
+    itemCategory: 'Osobni'
+
+  }
+
+] as any;
+
+dnes: number = Date.now()
+
   constructor() { }
 
   ngOnInit() {
