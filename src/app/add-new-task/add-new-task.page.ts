dc37c9e3728aad25ead23c31c297db1530e2e2b9
Nikola Valvodová <nikol.val.vodova@gmail.com>
2023-02-02 19:27:58 +0100

    priorit přidaná

--- src/app/add-new-task/add-new-task.page.ts
+++ src/app/add-new-task/add-new-task.page.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-add-new-task',
   templateUrl: './add-new-task.page.html',
   styleUrls: ['./add-new-task.page.scss'],
 })
 export class AddNewTaskPage implements OnInit {
+
+  
+  taskName: any
+  taskPriority: any
+
 
   constructor(public modalCtrl: ModalController) { }
 
@@ -16,4 +22,6 @@ export class AddNewTaskPage implements OnInit {
   async dismis(){
   await this.modalCtrl.dismiss()
   }
+
+
 }
